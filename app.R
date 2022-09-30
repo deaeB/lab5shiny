1a8b925be74bbb21f9f52969fbf9b7c57a613681
NI D.W <deaeb@hotmail.com>
2022-09-30 04:25:39 +0200

    add select

--- app.R
+++ app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(lab5package)
 
 ui <- fluidPage(
   titlePanel("getcityid"),
@@ -7,17 +8,32 @@ ui <- fluidPage(
     sidebarPanel(
       textInput(
         inputId = "name", label = "name of munuicipality", value = "", placeholder = "input a municipality"
-        )
+        ),
+      
+      selectInput("select", h3("data GET"), 
+                  choices = list("full data GET" = 1, "less data" = 2), selected = 1)
+      
     ),
     mainPanel(
-      textOutput(outputId = "idreturn")
+      conditionalPanel(
+        condition = "input$select == 1", 
+        textOutput(outputId = "idreturn")
+      )
     )
   )
 )
 
 server <- function(input, output){
-  output$idreturn <- renderText(getcityid(input$name))
+  output$idreturn <- renderText(
+
+    if(input$select == 1){
+      getcityid(input$name)
+    }else    if(input$select == 2){
+      getcityid_lessdata(input$name)
+    }
     
+    )
+  
 }
 
 shinyApp(ui, server)
